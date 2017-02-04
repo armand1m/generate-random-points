49dbb1a2765858b4f7c15eff742181fccc41ae4d
Jeff Geerling <geerlingguy@mac.com>
2017-02-03 23:25:21 -0200

    ci(configuration): Travis, Coveralls and semantic-release

--- index.js
+++ index.js
@@ -6,45 +6,48 @@
  * @param {number} count Number of points to generate.
  * @return {array} Array of Objects with lat and lng attributes.
  */
-function generateRandomPoints(center, radius, count) {
-  var points = [];
+function generateRandomPoints (center, radius, count) {
+  var points = []
 
   for (var i = 0; i < count; i++) {
-    points.push(generateRandomPoint(center, radius));
+    points.push(generateRandomPoint(center, radius))
   }
 
-  return points;
+  return points
 }
 
 /**
  * Generates number of random geolocation points given a center and a radius.
  *
  * Reference URL: http://goo.gl/KWcPE.
- * @param  {Object} center A JS object with lat and lng attributes.
+ * @param  {Object} center A JS object with 'latitude' and 'longitude' attributes.
  * @param  {number} radius Radius in meters.
- * @return {Object} The generated random points as JS object with lat and lng attributes.
+ * @return {Object} The generated random points as JS object with latitude and longitude attributes.
  */
-function generateRandomPoint(center, radius) {
-  var x0 = center.lng;
-  var y0 = center.lat;
+function generateRandomPoint (center, radius) {
+  var x0 = center.longitude
+  var y0 = center.latitude
 
   // Convert Radius from meters to degrees.
-  var rd = radius / 111300;
+  var rd = radius / 111300
 
-  var u = Math.random();
-  var v = Math.random();
+  var u = Math.random()
+  var v = Math.random()
 
-  var w = rd * Math.sqrt(u);
-  var t = 2 * Math.PI * v;
-  var x = w * Math.cos(t);
-  var y = w * Math.sin(t);
+  var w = rd * Math.sqrt(u)
+  var t = 2 * Math.PI * v
+  var x = w * Math.cos(t)
+  var y = w * Math.sin(t)
 
-  var xp = x / Math.cos(y0);
+  var xp = x / Math.cos(y0)
 
   return {
-    'lat': y + y0,
-    'lng': xp + x0
-  };
+    latitude: y + y0,
+    longitude: xp + x0
+  }
 }
 
-module.exports = generateRandomPoints
+module.exports = {
+  generateRandomPoint,
+  generateRandomPoints
+}
--- test/index.js
+++ test/index.js
@@ -1,12 +1,37 @@
-const generateRandomPoints = require('../');
-const { expect } = require('chai');
+const { generateRandomPoints, generateRandomPoint } = require('../')
+const chai = require("chai");
 
-describe("generate-random-points", function() {
-  it("should generate 1000 random points", () => {
-    let result = generateRandomPoints({'lat':24.23, 'lng':23.12}, 1000, 10)
+const { expect } = chai;
 
-    expect()
+chai.should();
+chai.use(require('chai-things'));
 
-    console.log(result)
-  });
-});
+const CENTER_POSITION = {
+  latitude: 24.23,
+  longitude: 23.12
+}
+
+const RADIUS = 1000
+
+const NEEDED_KEYS = ['latitude', 'longitude']
+
+describe('module: generate-random-points', function () {
+  describe('function: generateRandomPoint(center, radius)', function () {
+    it('should generate 1 random point', () => {
+      let result = generateRandomPoint(CENTER_POSITION, RADIUS)
+
+      expect(result).to.have.keys(NEEDED_KEYS)
+    })
+  })
+
+  describe('function: generateRandomPoints(center, radius, count)', function () {
+    it('should generate 10 random unique points', () => {
+      let result = generateRandomPoints(CENTER_POSITION, RADIUS, 100)
+
+      expect(result).to.be.an('array')
+
+      NEEDED_KEYS.forEach(key => result.should.all.have.property(key))
+    })
+  })
+})
+
